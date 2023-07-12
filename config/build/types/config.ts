e4cad3d4f4fb0e60bb151ffca1b5a97dfb711f04
Martynov Sergey <SerOleMar.dev@gmail.com>
2023-07-12 12:24:05 +1000

    feat: complete lesson #003

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -1,4 +1,11 @@
 export type BuildMode = "production" | "development";
+export type BuildPort = number;
+export type BuildIsDev = boolean;
+
+export interface BuildEnv {
+  mode: BuildMode;
+  port: BuildPort;
+}
 
 export interface BuildPaths {
   entry: string;
@@ -8,5 +15,7 @@ export interface BuildPaths {
 
 export interface BuildOptions {
   mode: BuildMode;
+  isDev: BuildIsDev;
   paths: BuildPaths;
+  port: BuildPort;
 }
