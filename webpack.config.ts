8e4f57dfd0b495c0bb5151e96efa577910f73b27
Martynov Sergey <SerOleMar.dev@gmail.com>
2023-10-29 14:08:58 +1000

    feat: complete lesson #004

--- webpack.config.ts
+++ webpack.config.ts
@@ -5,7 +5,7 @@ import buildConfig, { BuildEnv, BuildPaths } from "./config/webpack";
 export default (env: BuildEnv): webpack.Configuration => {
   const paths: BuildPaths = {
     build: path.resolve(__dirname, "dist"),
-    entry: path.resolve(__dirname, "src", "index.ts"),
+    entry: path.resolve(__dirname, "src", "index.tsx"),
     html: path.resolve(__dirname, "public", "index.html"),
   };
 
