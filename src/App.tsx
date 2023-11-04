341961af37ddf6946be7452f1ae1a9adca3652ed
Martynov Sergey <SerOleMar.dev@gmail.com>
2023-11-04 10:17:54 +1000

    feat: complete lesson #008

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
+import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { useTheme } from "./theme/useTheme";
 import "./styles/index.scss";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <button onClick={toggleTheme}>Toggle theme</button>
       <Link to={"/"}>Home</Link>
       <Link to={"/about"}>About</Link>
